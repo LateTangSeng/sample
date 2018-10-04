1f7bd66d1b1a8ebe1a6fcdcd6fcb5cf8ca87a1d8
LateTangSeng <shenglong2760@gmail.com>
2018-10-04 08:19:02 +0000

    Finish user edit, update, indexm and destory actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -38,5 +38,13 @@ class SessionsController extends Controller
     }
 
 
+    public function __construct()
+    {
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+
 
 }
